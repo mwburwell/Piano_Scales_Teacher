cefe01af8564c1505919ce3689564e3bb24d28bb
mwburwell <mwburwell@gmail.com>
2021-05-12 15:29:12 -0400

    Building a display for piano testing

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include "Scale.h"
+#include "Chord.hpp"
 #include "Keyboard.hpp"
 
 using namespace std;
@@ -12,19 +12,45 @@ using namespace std;
 using namespace std;
 
 int main(){
-    // Scale scale(Note::C, ScaleType::IONIAN);
 
-    // cout << "Scale size: " << scale.size() << endl;
-    // for(int i = 0; i < scale.size(); i++){
-    //     cout << "Note: " << scale.getNote_toString(i) << endl;
-    // }
+    std::vector<Chord*> chordProgression;
+    chordProgression.push_back(new Triad(Note::C, ScaleType::IONIAN));
+    Scale scale(Note::C, ScaleType::IONIAN);
 
     Keyboard piano(Note::A, 88);
-    for(unsigned int i = 0; i < piano.getNumberOfKeys(); i++){
+    for(unsigned int i = 0; i < piano.getSize(); i++){
         if(piano.getNote(i) == Note::C){
-            std::cout << std::endl;
+            std::cout << "X" << " ";
         }
-        std::cout << piano.getNote_toString(i) << " ";
+        else{
+            std::cout << "_" << " ";
+        }
+    }
+
+    std::cout << std::endl;
+
+    for(unsigned int i = 0; i < (int)(piano.getSize() / 2); i++){
+        if(piano.getNote(i) == chordProgression.at(0)->getNote(0)){
+            std::cout << "1" << " ";
+        }
+        else if(piano.getNote(i) == chordProgression.at(0)->getNote(1)){
+            std::cout << "2" << " ";
+        }
+        else if(piano.getNote(i) == chordProgression.at(0)->getNote(2)){
+            std::cout << "3" << " ";
+        }
+        else{
+            std::cout << "_" << " ";
+        }
+    }
+
+    for(unsigned int i = (int)(piano.getSize() / 2); i < piano.getSize(); i++){
+        if(scale.contains(piano.getNote(i))){
+            std::cout << "X" << " ";
+        }
+        else{
+            std::cout << "_" << " ";
+        }
     }
 
     return 0;
