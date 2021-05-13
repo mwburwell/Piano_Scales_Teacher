59ef747ced70ee205d8bda2cc44dac970a80a9b2
mwburwell <mwburwell@gmail.com>
2021-05-12 20:11:33 -0400

    built a CSV_Reader in Main then moved to it's own file

--- main.cpp
+++ main.cpp
@@ -1,57 +1,65 @@
 #include <iostream>
 #include <string>
+#include <map>
 #include <vector>
 #include "Chord.hpp"
 #include "Keyboard.hpp"
-
-using namespace std;
-
-// {Note::FFlat, "F-flat"},
+#include "CSV_Reader.hpp"
 
 
-using namespace std;
 
 int main(){
 
-    std::vector<Chord*> chordProgression;
-    chordProgression.push_back(new Triad(Note::C, ScaleType::IONIAN));
-    Scale scale(Note::C, ScaleType::IONIAN);
+    CSV_Reader reader("chordprogressions.csv");
 
-    Keyboard piano(Note::A, 88);
-    for(unsigned int i = 0; i < piano.getSize(); i++){
-        if(piano.getNote(i) == Note::C){
-            std::cout << "X" << " ";
-        }
-        else{
-            std::cout << "_" << " ";
-        }
+    for(int i = 0; i < reader.getSize(); i++){
+        
     }
 
-    std::cout << std::endl;
 
-    for(unsigned int i = 0; i < (int)(piano.getSize() / 2); i++){
-        if(piano.getNote(i) == chordProgression.at(0)->getNote(0)){
-            std::cout << "1" << " ";
-        }
-        else if(piano.getNote(i) == chordProgression.at(0)->getNote(1)){
-            std::cout << "2" << " ";
-        }
-        else if(piano.getNote(i) == chordProgression.at(0)->getNote(2)){
-            std::cout << "3" << " ";
-        }
-        else{
-            std::cout << "_" << " ";
-        }
-    }
+    // file.close();
+    // std::vector<int> chordProgressionStructure = {};
+    // Note key = Note::C;
+    // Keyboard piano(Note::A, 88);
+    // Scale scale(key, ScaleType::IONIAN);
 
-    for(unsigned int i = (int)(piano.getSize() / 2); i < piano.getSize(); i++){
-        if(scale.contains(piano.getNote(i))){
-            std::cout << "X" << " ";
-        }
-        else{
-            std::cout << "_" << " ";
-        }
-    }
+    // std::vector<Chord*> chordProgression;
+    // chordProgression.push_back(new Triad(key, ScaleType::IONIAN));
+
+    //  for(unsigned int i = 0; i < piano.getSize(); i++){
+    //     if(piano.getNote(i) == Note::C){
+    //         std::cout << "X" << " ";
+    //     }
+    //     else{
+    //         std::cout << "_" << " ";
+    //     }
+    // }
+
+    // std::cout << std::endl;
+
+    // for(unsigned int i = 0; i < (int)(piano.getSize() / 2); i++){
+    //     if(piano.getNote(i) == chordProgression.at(0)->getNote(0)){
+    //         std::cout << "1" << " ";
+    //     }
+    //     else if(piano.getNote(i) == chordProgression.at(0)->getNote(1)){
+    //         std::cout << "2" << " ";
+    //     }
+    //     else if(piano.getNote(i) == chordProgression.at(0)->getNote(2)){
+    //         std::cout << "3" << " ";
+    //     }
+    //     else{
+    //         std::cout << "_" << " ";
+    //     }
+    // }
+
+    // for(unsigned int i = (int)(piano.getSize() / 2); i < piano.getSize(); i++){
+    //     if(scale.contains(piano.getNote(i))){
+    //         std::cout << "X" << " ";
+    //     }
+    //     else{
+    //         std::cout << "_" << " ";
+    //     }
+    // }
 
     return 0;
 }
